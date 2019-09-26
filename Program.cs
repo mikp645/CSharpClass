36f9a48499d9082555bbb5518541af73a345d7b8
mikep645 <newmike20011@gmail.com>
2019-09-26 10:58:15 -0500

    lesson7operators

--- Program.cs
+++ Program.cs
@@ -9,19 +9,37 @@ namespace CSharpClass
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello World!");
-          // MyLesson5Examples();  
-          MyLesson6Examples();        
+            // Console.WriteLine("Hello World!");
+            // MyLesson5Examples();  
+            // MyLesson6Examples();
+            MyLesson7Examples();
         } // End Method Main 
 
-        static void MyLesson6Examples()
-            {
-           Lesson6 mySix = new Lesson6();
-           mySix.MyTrimExample();
+        static void MyLesson7Examples()
+        {
+            Lesson7 mySeven = new Lesson7();
+            mySeven.BasicMath();
+            mySeven.BasicModulus();
+            mySeven.MyCheckOperator1();
+            mySeven.MyCheckOperator2();
+            mySeven.MyLeftShiftOperator();
+            mySeven.MyRightShiftOperator(); 
+            mySeven.MyAndAssignmentOperator();            
+            mySeven.MyOrAssignmentOperator(); 
+            mySeven.MyXOrAssignmentOperator();
+            mySeven.MyIncrementDecrementExample();   
+        }  // end method my lesson 7 examples
+
+           static void MyLesson6Examples()
+
+
+        {
+            Lesson6 mySix = new Lesson6();
+            mySix.MyTrimExample();
             mySix.MyEqualsExample();
             mySix.MyLowerUpperExample();
             mySix.MyStringLength();
-            string myJoined = mySix.MyJoinedStrings("Happy","Coding");
+            string myJoined = mySix.MyJoinedStrings("Happy", "Coding");
             Console.WriteLine(myJoined);
             string myFav = mySix.MyStringBuilder("pizza", 3);
             Console.WriteLine(myFav);
@@ -38,7 +56,7 @@ namespace CSharpClass
             myfive.ConvertFloatToInt();
             myfive.Longfromint();
         }// end method my lesson 5 examples
-        
 
-        } //End class
+
+    } //End class
 } //end namespace
